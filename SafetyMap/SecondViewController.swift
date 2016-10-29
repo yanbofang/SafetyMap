42ab012c2bc48cbc489445f6ceb5bc1949e6bf63
yanbofang <yanbo.fang@duke.edu>
2016-10-29 11:32:26 -0400

    Initial Commit

---
+++ SafetyMap/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SafetyMap
+//
+//  Created by Yanbo Fang on 10/29/16.
+//  Copyright © 2016 Yanbo Fang. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
