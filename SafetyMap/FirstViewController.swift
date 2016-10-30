c5ca23bd89e4c51cecab94297090e31adab5a85f
Minh Tran <thuba2011@gmail.com>
2016-10-29 22:45:08 -0400

    added receiver notification

--- SafetyMap/FirstViewController.swift
+++ SafetyMap/FirstViewController.swift
@@ -17,6 +17,54 @@ class FirstViewController: UIViewController, AGSCalloutDelegate, AGSWebMapDelega
     var webmap: AGSWebMap!
     var popups = [AGSPopup]()
     
+    struct Notification {
+        var name: String
+        var lat: Double
+        var lng : Double
+    }
+    
+    var receiver_id: Int = 0 {
+        didSet {
+            downloadData()
+        }
+    }
+    var notifications = [Notification]()
+    
+    func downloadData() {
+        let url = NSURL(string: "http://hoopsapp.netai.net/safe/receiver.php?receiver_id=2" + String(receiver_id))
+        let request = URLRequest(url: url as! URL)
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data else {
+                print("request failed \(error)")
+                return
+            }
+            
+            do {
+                if let json = try JSONSerialization.jsonObject(with: data) as?
+                    [AnyObject] {
+                    for index in 0...json.count-1 {
+                        if let item = json[index] as? [String: AnyObject] {
+                            if let name = item["name"] as? String {
+                                self.notifications.append(Notification(
+                                    name: item["name"] as! String,
+                                    lat: Double(item["lat"] as! String)!,
+                                    lng: Double(item["lng"] as! String)!
+                                ))
+                            }
+                        }
+                    }
+                }
+            } catch let parseError {
+                print("parsing error: \(parseError)")
+                let responseString = String(data: data, encoding: .utf8)
+                print("raw response: \(responseString)")
+            }
+        }
+        task.resume()
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.mapView.callout.delegate = self
